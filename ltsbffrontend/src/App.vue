2c9b049289b0231b0cef10f09e40d1b53f268f81
ryualvin <lsben1014@gmail.com>
2026-05-16 14:38:58 +0900

    去除页面上边距

--- ltsbffrontend/src/App.vue
+++ ltsbffrontend/src/App.vue
@@ -16,6 +16,6 @@ import BasicLayout from "./layouts/BasicLayout.vue";
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  //margin-top: 60px;
 }
 </style>
